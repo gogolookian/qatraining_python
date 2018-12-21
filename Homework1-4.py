9d2ea4f484315320fd855e8eb2b6eeed0e4a96b7
Ian <ian@gogolook.com>
2018-12-21 16:56:11 +0800

    Homework1-3, 1-4 have been completed

--- Homework1-4.py
+++ Homework1-4.py
@@ -4,7 +4,13 @@ def printer(number):
             print(' ',end='')
         for k in range(0,(i*2)+1):
             print('*',end='')
-        print('')    
+        print('')
+    for a in range(0,number-1):
+        for b in range(0,a+1):
+            print(' ',end='')
+        for c in range(0,number*2-a*2-3):
+            print('*',end='')    
+        print('')
 
 if __name__ == "__main__":
     a = input('請輸入一個整數：')
