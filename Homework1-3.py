c77211db62e6f4720f53ec740be78db61cd484ff
Ian <ian@gogolook.com>
2018-12-21 16:01:20 +0800

    Homework1-3 has been resolved.

--- Homework1-3.py
+++ Homework1-3.py
@@ -2,9 +2,10 @@ def printer(number):
     for i in range(0,number):
         for j in range(0,number-i-1):
             print(' ',end='')
-
         for k in range(0,i+1):
             print('*',end='')
+        for l in range(0,i):
+            print('*',end='')    
         print('')    
 
 
