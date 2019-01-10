e368e8de004499f08a47aedd3010090d066e7d3e
Ian <ian@gogolook.com>
2019-01-10 11:49:29 +0800

    Finish file_access.py

--- file_access.py
+++ file_access.py
@@ -1,5 +1,6 @@
 import re
 import json
+import operator
 
 logFile = open("access.log", "r")
 uidFile = open("uid.log", "w+")
@@ -10,13 +11,24 @@ for line in logFile.readlines():
     if match:
         # print("Found UID: " + match.group(1))
         uidFile.write(match.group(1) + "\n")
+        uid = uidFile.read(10)
+        print(uid)
     else:
         pass
-
-
-print(uidFile.read())
-
-
-
 logFile.close()
+uidFile.close()
+
+uidFile = open("uid.log", "r")
+
+dictionary_uid_counts = {}
+
+for uidLine in uidFile.readlines():
+    if not(uidLine in dictionary_uid_counts):
+        dictionary_uid_counts[uidLine] = 1
+    else:
+        dictionary_uid_counts[uidLine] = int(dictionary_uid_counts.get(uidLine)) + 1
+
+topFiveDictionary = dict(sorted(dictionary_uid_counts.items(), key=operator.itemgetter(1), reverse=True)[:5])
+
+print(topFiveDictionary)
 
